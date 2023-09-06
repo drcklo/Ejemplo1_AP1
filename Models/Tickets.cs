213c7fbfcbd4ccab7cbed353ffc87daab3f16702
darcklito <luis_paulino5@ucne.edu.do>
2023-09-06 14:30:39 -0400

    Menu Registrar Prioridades completado

---
+++ Models/Tickets.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketApp.Shared;
+
+public class Tickets
+{
+	[Key]
+	public int TicketId { get; set; }
+
+	public DateTime Fecha { get; set; }
+
+	public int ClienteId { get; set; }
+
+	[Required(ErrorMessage = "Solicitado es requerido")]
+	public string? SolicitadoPor { get; set; }
+
+	[Required(ErrorMessage = "El Asunto es requerido")]
+	public string? Asunto { get; set; }
+
+	[Required(ErrorMessage = "La descripción es requerida")]
+	public string? Descripcion { get; set; }
+
+}
